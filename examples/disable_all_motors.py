6f4f64d382a60ce3c462eb48fdda756ec149975b
Qin Yihua <qinyh10300@gmail.com>
2025-02-24 17:59:26 +0800

    add disable_all_motors.py

---
+++ examples/disable_all_motors.py
@@ -0,0 +1,17 @@
+import time
+from fourier_grx_client import RobotClient, ControlGroup
+from loguru import logger
+
+if __name__ == "__main__":
+    # Enable the robot motors
+    client = RobotClient(namespace="gr/my_awesome_robot", server_ip="192.168.137.252")
+    client.enable()
+    logger.info("Motors enabled")
+    time.sleep(1)
+
+    # Disable the robot motors
+    client.disable()
+    logger.info("Motors disabled")
+
+    # Close the connection to the robot server
+    client.close()
